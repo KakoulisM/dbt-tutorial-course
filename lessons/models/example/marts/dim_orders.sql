42cb87dba884f646a7b3e174ffd8d1a6def04497
Minas <kakoulisminas@hotmail.com>
2025-07-23 11:08:01 +0200

    after blackout, small changes to normalize

--- lessons/models/example/marts/dim_orders.sql
+++ lessons/models/example/marts/dim_orders.sql
@@ -7,8 +7,7 @@ order_item_measures AS (
     SUM(sale_price) AS total_sale_price,
     SUM(product_cost) AS total_product_cost,
     SUM(item_profit) AS total_profit,
-    SUM(item_discount) AS total_discount,
-
+    SUM(item_discount) AS total_discount
   FROM {{ ref('int_ecommerce__order_items_products') }}
   GROUP BY 1
 )
@@ -24,8 +23,11 @@ SELECT
   om.total_sale_price,
   om.total_product_cost,
   om.total_profit,
-  om.total_discount
+  om.total_discount,
+  TIMESTAMP_DIFF(od.created_at, user_data.first_order_created_at, DAY) AS days_since_first_order
 
 FROM {{ ref('stg_ecommerce__orders') }} AS od
 LEFT JOIN order_item_measures AS om
   ON od.order_id = om.order_id
+LEFT JOIN {{ ref('int_ecommerce__first_order_created') }} AS user_data
+  ON user_data.user_id = od.user_id
